3a62bdf1188e6a1b599909d9887626585ec729ba
Ian <ian.mosonik@student.moringaschool.com>
2024-12-17 17:47:54 +0300

    creating database

---
+++ connect_db.py
@@ -0,0 +1,69 @@
+from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+ 
+
+DATABASE_URL = "sqlite:///movies.db"
+ 
+
+engine = create_engine(DATABASE_URL)
+ 
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+ 
+
+Base = declarative_base()
+ 
+
+class Director(Base):
+    __tablename__ = 'directors'
+ 
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    birth_year = Column(Integer)
+ 
+    movies = relationship("Movie", back_populates="director")
+ 
+    def __repr__(self):
+        return f"<Director(name='{self.name}', birth_year='{self.birth_year}')>"
+ 
+
+class Movie(Base):
+    __tablename__ = 'movies'
+ 
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    director_id = Column(Integer, ForeignKey('directors.id'))
+    year = Column(Integer)
+    rating = Column(Float)
+ 
+    director = relationship("Director", back_populates="movies")
+ 
+    def __repr__(self):
+        return f"<Movie(title='{self.title}', year='{self.year}', rating='{self.rating}')>"
+ 
+
+Base.metadata.create_all(bind=engine)
+ 
+
+db = SessionLocal()
+ 
+
+new_director = Director(name="Christopher Nolan", birth_year=1970)
+db.add(new_director)
+db.commit()
+ 
+
+new_movie = Movie(title="Inception", director_id=new_director.id, year=2010, rating=8.8)
+db.add(new_movie)
+db.commit()
+ 
+
+directors = db.query(Director).all()
+for director in directors:
+    print(director)
+    for movie in director.movies:
+        print(movie)
+ 
+
+db.close()
